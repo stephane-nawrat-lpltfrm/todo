d2b3d6e61b8cced5db2dae2f53db96d9f6e0695b
Stéphane Nawrat <stephane.nawrat@laplateforme.io>
2026-04-15 15:02:40 +0200

    feat: implement dynamic rendering with todos map and template literals

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./style.css";
+
+// 1. Correction de la casse sur querySelector
+const ul = document.querySelector("ul");
+
+const todos = [
+  {
+    text: "je suis une todo",
+    done: false,
+  },
+  {
+    text: "faire du JavaScript",
+    done: true,
+  },
+];
+
+const displayTodo = () => {
+  const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo, index);
+  });
+  ul.innerHTML = "";
+  ul.append(...todosNode);
+};
+
+const createTodoElement = (todo, index) => {
+  const li = document.createElement("li");
+
+  // 2. Ajout des backticks (`) pour le template HTML
+  li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"></span>
+    <p>${todo.text}</p>
+    <button>Supprimer</button>
+  `;
+  return li;
+};
+
+displayTodo();
