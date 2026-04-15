800e03b71c31ff0773974eec5a07cb01b6cc6654
Stéphane Nawrat <stephane.nawrat@laplateforme.io>
2026-04-15 16:22:03 +0200

    feat: implement form submission and state-driven todo creation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import "./style.css";
 
-// 1. Correction de la casse sur querySelector
 const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
+console.log(form, input);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addTodo(value);
+  displayTodo();
+});
 
 const todos = [
   {
@@ -25,13 +36,19 @@ const displayTodo = () => {
 const createTodoElement = (todo, index) => {
   const li = document.createElement("li");
 
-  // 2. Ajout des backticks (`) pour le template HTML
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>
     <button>Supprimer</button>
   `;
   return li;
+};
+
+const addTodo = (text) => {
+  todos.push({
+    text,
+    done: false,
+  });
 };
 
 displayTodo();
